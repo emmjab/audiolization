65da1aa4ba2d2c0f868ad5fc8faf5e536c648ee1
ayota <ayota@outlook.com>
2015-07-12 11:04:00 -0400

    add audio graph + button to audioliz page

--- experiments/maketones.py
+++ experiments/maketones.py
@@ -2,7 +2,7 @@
 from scipy.io.wavfile import write
 import numpy as np
 
-duration=5
+duration=2
 amp=1E4
 rate=44100
 
@@ -12,12 +12,15 @@ def note(freq, duration, amp, rate):
     return data.astype(np.int16) # two byte integers
 
 tone0 = note(0, 1, amp, rate) #silence
-tone1 = note(261.63, 2, amp, rate) # C4
-tone2 = note(329.63, 1, amp, rate) # E4
-tone3 = note(392.00, 4, amp, rate) # G4
+tone1 = note(261.63, duration, amp, rate) # C4
+tone2 = note(277.18, duration, amp, rate) # E4
+tone3 = note(293.66, duration, amp, rate) # G4
+tone4 = note(311.13, duration, amp, rate) # C1
+tone5 = note(329.63, duration, amp, rate) # D1
+tone6 = note(349.23, duration, amp, rate) # Dsharp1
 
 
-seq1 = np.concatenate((tone0,tone1,tone2,tone3, tone1),axis=1)
+seq1 = np.concatenate((tone0,tone1, tone2, tone3, tone4,tone5,tone6, tone0),axis=1)
 seq2 = np.concatenate((tone0,tone2,tone0,tone0, tone2),axis=1)
 seq3 = np.concatenate((tone0,tone0,tone3,tone0, tone3),axis=1)
 
