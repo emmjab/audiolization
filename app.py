4d9bf95dd014f7d4d79297d15a2469f09a8a8f26
emmjab <e.jablonski@gmail.com>
2015-07-12 16:50:30 -0400

    Adding everything else

---
+++ app.py
@@ -0,0 +1,105 @@
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+from werkzeug import secure_filename
+import os
+import datetime
+
+# configuration
+DATABASE = 'audiolization.db' # think about where to put this on the server
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+UPLOAD_FOLDER = 'static/csv'
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
+# create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_db()
+    g.db.row_factory = sqlite3.Row
+    return g.db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+        return redirect(url_for('upload_file', filename=filename))
+
+    else:
+        return render_template('index.html')
+
+@app.route('/test', methods=['GET', 'POST'])
+def upload_file():
+
+    return render_template('audiolizations.html')
+
+@app.route('/sample', methods=['GET','POST'])
+def sample():
+    return render_template('sample.html')
+# @app.route('/test', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return redirect(url_for('upload_file',
+#                                     filename=filename))
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form action="" method=post enctype=multipart/form-data>
+#       <p><input type=file name=file>
+#          <input type=submit value=Upload>
+#     </form>
+#     '''
+
+
+if __name__ == '__main__':
+   # init_db()
+    app.run()
+
